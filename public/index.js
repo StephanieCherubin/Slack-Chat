393770b0660a05ffb0506a66e5b1bf5a21efc01b
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-04-18 23:08:32 -0700

    users can join chat and send messages

--- public/index.js
+++ public/index.js
@@ -16,11 +16,36 @@ $(document).ready(()=>{
     }
   });
 
+  $('#sendChatBtn').click((e) => {
+    e.preventDefault();
+    // Get the message text value
+    let message = $('#chatInput').val();
+    // Make sure it's not empty
+    if(message.length >){
+      // Emit the message with the current user to the server
+      socket.emit('new message', {
+        sender : currentUser,
+        message : message,
+      });
+      $('#chatInput').val("");
+    }
+  });
+
   //socket listeners
   socket.on('new user', (username) => {
     console.log(`${username} has joined the chat`);
     // Add the new user to the online users div
     $('.usersOnline').append(`<div class="userOnline">${username}</div>`);
+  })
+
+  // output the new message
+  socket.on('new message', (data) => {
+    $('.messageContainer').append(`
+      <div class="message">
+        <p class="messageUser">${data.sender}: </p>
+        <p class="messageText">${data.message}</p>
+      </div>
+    `);
   })
 
 })
