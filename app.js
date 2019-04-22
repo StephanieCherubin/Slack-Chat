43f09a6330bf6509936a4e9d5b4f632a09358e7c
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-04-21 21:41:50 -0700

    record when new user is connected

--- app.js
+++ app.js
@@ -4,19 +4,23 @@ const server = require('http').Server(app);
 
 //Socket.io
 const io = require('socket.io')(server);
+
 // We'll store our online users here
 let onlineUsers = {};
+
 //Save the channels in this object.
 let channels = {"General" : []}
 
 io.on("connection", (socket) => {
   // Make sure to send the users to our chat file
   // This file will be read on new socket connections
+  console.log('🔌 New user connected! 🔌');
   require('./sockets/chat.js')(io, socket, onlineUsers, channels);
 })
 
 //Express View Engine for Handlebars
 const exphbs  = require('express-handlebars');
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 //Establish your public folder
